f1457aafc2adc3a580910ccef00a1a0e2e1d18cb
zenitpobedit7 <zenitpobedit7@gmail.com>
2025-10-01 15:46:14 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+﻿# Конфигурация бота Valery Cake Lab
+
+# Токен бота от @BotFather
+BOT_TOKEN = "8106166938:AAFdGpmv8qpju0zfQ0gGIA1KdGtNrUqNf7M"
+
+# Username канала (с @)
+CHANNEL_USERNAME = "@ValeryCakeLab"
+
+# Username для связи (с @)
+CONTACT_USERNAME = "@CakeValery"
